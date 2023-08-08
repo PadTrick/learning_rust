dae85e310b9858c0b52cbd8a9bdfcb0ca1243b54
PadTrick <my_mak@web.de>
2023-08-08 09:13:02 +0200

    finished pong.

---
+++ pong/README.md
@@ -0,0 +1,7 @@
+#Pong
+Pong finshed, but still room for improvements.
+- Tweak the constans to change how the game feels to play.
+- Make the paddles have a velocity, so the player can have more fine-grained control over their movement.
+- Add a score counter, and make the field reset after a ball goes offscreen.
+- Add some cool effects, or replace sprites.
+- Rewrite the game using an ECS library like Specs, Legion or Hecs.
--- pong/src/main.rs
+++ pong/src/main.rs
@@ -1,4 +1,4 @@
-use tetra::graphics::{self, Color, Texture};
+use tetra::graphics::{self, Color, Texture, Rectangle};
 use tetra::input::{self, Key};
 use tetra::math::Vec2;
 use tetra::window;
@@ -7,15 +7,51 @@ use tetra::{Context, ContextBuilder, State};
 const WINDOW_WIDTH: f32 = 640.0;
 const WINDOW_HEIGHT: f32 = 480.0;
 const PADDLE_SPEED: f32 = 8.0;
+const BALL_SPEED: f32 = 5.0;
+const PADDLE_SPIN: f32 = 4.0;
+const BALL_ACC: f32 = 0.05;
 
 struct Entity {
     texture: Texture,
     position: Vec2<f32>,
+    velocity: Vec2<f32>,
 }
 
 impl Entity {
     fn new(texture: Texture, position: Vec2<f32>) -> Entity {
-        Entity { texture, position }
+        Entity::with_velocity(texture, position, Vec2::zero())
+    }
+
+    fn with_velocity(texture: Texture, position: Vec2<f32>, velocity: Vec2<f32>) -> Entity {
+        Entity { 
+            texture,
+            position,
+            velocity,
+        }
+    }
+
+    fn width(&self) -> f32 {
+        self.texture.width() as f32
+    }
+
+    fn height(&self) -> f32 {
+        self.texture.height() as f32
+    }
+
+    fn bounds(&self) -> Rectangle {
+        Rectangle::new(
+            self.position.x,
+            self.position.y,
+            self.width(),
+            self.height(),
+        )
+    }
+
+    fn centre(&self) -> Vec2<f32> {
+        Vec2::new(
+            self.position.x + (self.width() / 2.0),
+            self.position.y + (self.height() / 2.0),
+        )
     }
 }
 
@@ -45,10 +81,12 @@ impl GameState {
             WINDOW_HEIGHT / 2.0 - ball_texture.height() as f32 / 2.0,
         );
 
+        let ball_velocity = Vec2::new(-BALL_SPEED, 0.0);
+
         Ok(GameState { 
             player1: Entity::new(player1_texture, player1_position),
             player2: Entity::new(player2_texture, player2_position),
-            ball: Entity::new(ball_texture, ball_position),
+            ball: Entity::with_velocity(ball_texture, ball_position, ball_velocity)
         })
     }
 }
@@ -69,6 +107,48 @@ impl State for GameState {
 
         if input::is_key_down(ctx, Key::Down) {
             self.player2.position.y += PADDLE_SPEED;
+        }
+
+        self.ball.position += self.ball.velocity;
+
+        let player1_bounds = self.player1.bounds();
+        let player2_bounds = self.player2.bounds();
+        let ball_bounds = self.ball.bounds();
+
+        let paddle_hit = if ball_bounds.intersects(&player1_bounds) {
+            Some(&self.player1)
+        } else if ball_bounds.intersects(&player2_bounds) {
+            Some(&self.player2)
+        } else {
+            None
+        };
+
+        if let Some(paddle) = paddle_hit {
+            // Increase the ball's velocity, then flip it.
+            self.ball.velocity.x =
+                -(self.ball.velocity.x + (BALL_ACC * self.ball.velocity.x.signum()));
+
+            // Calculate the offset between the paddle and the ball, as a number between
+            // -1.0 and 1.0
+            let offset = (paddle.centre().y - self.ball.centre().y) / paddle.height();
+
+            // Apply the spin to the ball.
+            self.ball.velocity.y += PADDLE_SPIN * -offset;
+        }
+
+        if self.ball.position.y <= 0.0 || self.ball.position.y + self.ball.height() >= WINDOW_HEIGHT
+        {
+            self.ball.velocity.y = -self.ball.velocity.y;
+        }
+
+        if self.ball.position.x < 0.0 {
+            window::quit(ctx);
+            println!("Player 2 wins!");
+        }
+
+        if self.ball.position.x > WINDOW_WIDTH {
+            window::quit(ctx);
+            println!("Player 1 wins!");
         }
 
         Ok(())
